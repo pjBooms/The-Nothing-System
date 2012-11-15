3606ac79cb3f7b60e1de7b50865c4d61bb5cf2a6
kit <nlipsky@excelsior-usa.com>
2012-11-15 13:34:03 +0700

    Fixed bug in build% run scripts.
    System classes must be in a separate directory with sysDir to allow new classes recompilation within the system.

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 set JDK_DIR=C:\jdk1.6.0
-javac -classpath %JDK_DIR%/lib/tools.jar -sourcepath src -d sysDir src/com/excelsior/nothing/Main.java 
+mkdir sysclasses
+javac -classpath %JDK_DIR%/lib/tools.jar -sourcepath src -d sysclasses src/com/excelsior/nothing/Main.java 
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 set JDK_DIR=C:\jdk1.6.0
 pushd sysDir
-java -cp .;%JDK_DIR%/lib/tools.jar com.excelsior.nothing.Main
+java -cp ../sysclasses;%JDK_DIR%/lib/tools.jar com.excelsior.nothing.Main
 popd
